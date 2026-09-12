a9aca19180af3294a4795b6a09e6e9c8a53f5580
Ap-tech-23 <atharvtpawar2308@gmail.com>
2026-09-12 18:28:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-
 import { app } from "./firebase.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 
-console.log("Swift Desk Connected", app);
+const auth = getAuth(app);
+
+const email = document.querySelector('input[type="email"]');
+const password = document.querySelector('input[type="password"]');
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => alert("Login Successful"))
+    .catch((error) => alert(error.message));
+});
