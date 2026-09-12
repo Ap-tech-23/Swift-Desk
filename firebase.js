51662d149bb4426171eb01347146d75745b71343
Ap-tech-23 <atharvtpawar2308@gmail.com>
2026-09-12 10:23:50 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
-// Firebase configuration will be added later
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrVLt4T0fD_-OYntIirF1JH_XuLebtk3o",
+  authDomain: "swift-desk-77ecc.firebaseapp.com",
+  projectId: "swift-desk-77ecc",
+  storageBucket: "swift-desk-77ecc.firebasestorage.app",
+  messagingSenderId: "369501454519",
+  appId: "1:369501454519:web:ca92871b4c35bb62330d32"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
