86456877dd68ca13cda5688a3559b0d32370a13c
Ap-tech-23 <atharvtpawar2308@gmail.com>
2026-09-15 11:23:31 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 
-apiKey: "AIzaSyBrVLt4T0fD_-OYntIirF1JH_XuLebtk3o",
+const firebaseConfig = {
+  apiKey: "AIzaSyBrVLt4T0fD_-OYntIirF1JH_XuLebtk3o",
   authDomain: "swift-desk-77ecc.firebaseapp.com",
   projectId: "swift-desk-77ecc",
   storageBucket: "swift-desk-77ecc.firebasestorage.app",
@@ -8,8 +9,6 @@ apiKey: "AIzaSyBrVLt4T0fD_-OYntIirF1JH_XuLebtk3o",
   appId: "1:369501454519:web:ca92871b4c35bb62330d32"
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 
 export { app };
