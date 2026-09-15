9294ec5deed8b340f4da00cc45256b71f30645d1
Ap-tech-23 <atharvtpawar2308@gmail.com>
2026-09-15 11:07:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert("App loaded");
+
 import { app } from "./firebase.js";
 import {
   getAuth,
@@ -8,7 +8,7 @@ import {
   onAuthStateChanged,
   updateProfile
 } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
-
+alert("App loaded");
 const auth = getAuth(app);
 
 const emailInput = document.getElementById("email");
